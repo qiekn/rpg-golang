1b90c68c8eb15e127c855ba64a89fa75cb688130
qiekn <qiekn@kimidayo.cn>
2025-03-02 18:18:30 +0800

    ep2. drawing & moving sprites

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"log"
 
@@ -8,9 +9,27 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	PlayerImage *ebiten.Image
+	X, Y        float64
+}
 
 func (g *Game) Update() error {
+
+	// react to key presses
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		g.Y -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		g.Y += 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		g.X -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		g.X += 2
+	}
+
 	return nil
 }
 
@@ -18,7 +37,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	screen.Fill(color.RGBA{120, 180, 255, 255})
 
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	opts := ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(g.X, g.Y)
+
+	// draw out player
+	screen.DrawImage(
+		g.PlayerImage.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image),
+		&opts,
+	)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -29,7 +55,14 @@ func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	if err := ebiten.RunGame(&Game{}); err != nil {
+
+	PlayerImage, _, err := ebitenutil.NewImageFromFile("assets/images/ninja.png")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	if err := ebiten.RunGame(&Game{PlayerImage: PlayerImage, X: 100, Y: 100}); err != nil {
 		log.Fatal(err)
 	}
 }
